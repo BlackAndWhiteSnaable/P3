6c3abaf644ff1083debff87262ccffe3e1c0ad91
Paduchele007 <31723425+Paduchele007@users.noreply.github.com>
2017-11-30 20:42:14 +0100

    Update LED.c
    this code works on the msp430

--- LED.c
+++ LED.c
@@ -1,23 +1,19 @@
 #include <msp430x20x3.h>
+#define North           1
+#define East            2
+#define South           4
+#define West            8
+#define NorthEast       16
+#define SouthEast       32
+#define SouthWest       64
+#define NorthWest       128
 
 
-void main(void)
-{
-  WDTCTL = WDTPW | WDTHOLD;                             
-  unsigned volatile int step, counter;
-  step = 1;
-  counter = 0;
-  P1DIR |= 0x02;                                //p1.1 to B1            P3
-  P1DIR |= 0x04;                                //p1.2 to B2            P4       
-  P1DIR |= 0x08;                                //p1.3 to A2            P5
-  P1DIR |= 0x10;                                //p1.4 to A1            P6
-  P1DIR |= 0x20;                                //p1.5 tristate buffer, enable or disable pair of wheels
-  P1DIR |= 0x40;                                //p1.6 to tristate buffers, to select the direction of rotation
-  TACCR0 = 8000;                                //delay between the steps
-  TACTL = MC_1 | ID_0 | TASSEL_2 | TACLR;       //configure timer A0
-  P1OUT |= 0x20;                                //enable first pair of wheels
-  P1OUT |= 0x40;                                //choose direction
-  while(counter < 2000){                        //number of steps
+void stepping(void){
+  int step = 1, counter = 0;
+  CCR0 = 8000;
+  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
+  while(counter < 1000){                        //number of steps
     while((TACTL & 0x0001) == 0){}              //halfstepping from here down
     TACTL &= ~0x0001;
     if(step == 1){                      //A1
@@ -35,7 +31,7 @@ void main(void)
       P1OUT |= 0x10;
       step++;
       counter++;
-    }
+     }
     else if(step == 3){                 //B1 
       P1OUT |= 0x02;
       P1OUT &= ~0x04;
@@ -43,7 +39,7 @@ void main(void)
       P1OUT &= ~0x10;
       step++;
       counter++;
-      }
+     }
     else if(step == 4){                 //B1 A2
       P1OUT |= 0x02;
       P1OUT &= ~0x04;
@@ -75,7 +71,7 @@ void main(void)
       P1OUT &= ~0x10;
       step++;
       counter++;
-      }
+    }
     else if(step == 8){                 //B2 A1
       P1OUT &= ~0x02;
       P1OUT |= 0x04;
@@ -84,45 +80,71 @@ void main(void)
       step = 1;
       counter++;
     }
-  }                                   //end program
-  P1OUT &= ~0x02;
-  P1OUT &= ~0x04;
-  P1OUT &= ~0x08;
-  P1OUT &= ~0x10;
+  }
+}
+void moveNorthWest(void){
+  P1OUT |= 0x20;                                //enable the pair of wheels
+  P1OUT |= 0x40;                                //choose direction
+  stepping();                                     
+  P1OUT &= ~0x20;
+}
+void moveSouthEast(void){
+  P1OUT |= 0x20;                                //enable the pair of wheels
+  P1OUT &= ~0x40;                               //choose direction
+  stepping();
+  P1OUT &= ~0x20;
+}
+void moveNorthEast(void){
+  P1OUT |= 0x80;                                //enable the pair of wheels
+  P2OUT |= 0x80;                                //choose direction
+  stepping();
+  P1OUT &= ~0x80;
+}
+void moveSouthWest(void){
+  P1OUT |= 0x80;                                //enable the pair of wheels
+  P2OUT &= ~0x80;                               //choose direction
+  stepping();
+  P2OUT &= ~0x80;
+}
+void moveNorth(void){
+  P1OUT |= 0x20;                                //enable first pair of wheels
+  P1OUT |= 0x40;                                //select direction
+  P1OUT |= 0x80;                                //enable second pair of wheels
+  P2OUT |= 0x80;                                //select direction
+  stepping();
+  P1OUT &= ~0x20;
+  P1OUT &= ~0x80;
+}
+void moveWest(void){
+  P1OUT |= 0x80;                                //enable first pair of wheels
+  P2OUT &= ~0x80;                               //select direction
+  P1OUT |= 0x20;                                //enable second pair of wheels
+  P1OUT |= 0x40;                                //select direction
+  stepping();
+  P1OUT &= ~0x20;
+  P1OUT &= ~0x80; 
+}
+void moveSouth(void){
+  P1OUT |= 0x80;                                //enable first pair of wheels
+  P2OUT &= ~0x80;                               //select direction
+  P1OUT |= 0x20;                                //enable second pair of wheels
+  P1OUT &= ~0x40;                               //select direction
+  stepping();
+  P1OUT &= ~0x20;
+  P1OUT &= ~0x80;
+  
+}
+void moveEast(void){
+  P1OUT |= 0x80;                                //enable first pair of wheels
+  P2OUT |= 0x80;                                //select direction
+  P1OUT |= 0x20;                                //enable second pair of wheels
+  P1OUT &= ~0x40;                               //select direction
+  stepping();
+  P1OUT &= ~0x20;
+  P1OUT &= ~0x80;
 }
 
-/*
-#define N       1
-#define E       2
-#define S       4
-#define W       8
-#define NE      16
-#define SE      32
-#define SW      64
-#define NW      128
-
-WDTCTL = WDTPW | WDTHOLD;                       //Stop Watchdog Timer
-P1DIR |= 0x02;                                  //configure p1.1 P3 as output
-P1DIR |= 0x04;                                  //configure p1.2 P4 as output
-P1DIR |= 0x08;                                  //configure p1.3 P5 as ouptut
-P1DIR |= 0x10;                                  //configure p1.4 P6 as output
-P1DIR |= 0x20;                                  //configure p1.5 P7 as output
-P1DIR |= 0x40;                                  //configure p1.6 p8 as output
-P1DIR |= 0x80;                                  //configure p1.7 p9 as output
-P2DIR |= 0x40;                                  //configure p2.6    as output
-P2DIR |= 0x80;                                  //configure p2.7    as output          
-
-void main(void){
-  move(N);
-  move(E);
-  move(S);
-  move(W);
-  move(NE);
-  move(SE);
-  move(SW);
-  move(NW);
-}
-void move(uint8_t x){
+void move(unsigned volatile int x){
   switch(x){
   case 1: moveNorth(); break;
   case 2: moveEast(); break;
@@ -135,178 +157,48 @@ void move(uint8_t x){
   }
 }
 
-void moveNorth(){
-  TACCR0 = 5000;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 5000){
+void wait(void){
+  TACTL = MC_2 | ID_3 | TASSEL_2 |TACLR;
+  unsigned volatile int waitingTime = 0;
+  while(waitingTime < 10){
     while((TACTL & 0x0001) == 0){}
     TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
+    waitingTime++;
   }
 }
-void moveEast(){
-  TACCR0 = 5000;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 5000){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
-  }
-}
-void moveSouth(){
-  TACCR0 = 5000;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 5000){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
-  }
-}
-void moveWest(){
-  TACCR0 = 5000;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 5000){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
-  }
-}
-void moveNorthEast(){
-  TACCR0 = 5000;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 7500){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
-  }
-}
-void moveSouthEast(){
-  TACCR0 = 5000;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 7500){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
-  }
-}
-void moveSouthWest(){
-  TACCR0 = 7500;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 5000){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
-  }
-}
-void moveNorhtWest(){
-  TACCR0 = 7500;
-  TACTL = MC_1 | ID_0 | TASSEL_2 |TACLR;
-  volatile unsigned int step, counter;
-  step = 1;
-  counter = 0;
-  while(counter <= 5000){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){}
-    else if(step == 2){}
-    else if(step == 3){}
-    else if(step == 4){};
-  }
-}*/
 
-
-/*
-void main (void){
-  volatile unsigned int step, counter;          //declare variable for steps
-  step = 1;                                     //initialize variable for steps
-  counter = 0;
-  WDTCTL = WDTPW | WDTHOLD;                     //Stop Watchdog Timer
-  P1DIR |= 0x02;                                //configure p1.1 P3 as output
-  P1DIR |= 0x04;                                //configure p1.2 P4 as output
-  P1DIR |= 0x08;                                //configure p1.3 P5 as ouptut
-  P1DIR |= 0x10;                                //configure p1.4 P6 as output
-  //TACCR0 = 5000;
-  TACTL = MC_2 | ID_0 | TASSEL_1 | TACLR;       //configure timer A
-  while(counter < 100){
-    while((TACTL & 0x0001) == 0){}
-    TACTL &= ~0x0001;
-    if(step == 1){
-      P1OUT |= 0x02;
-      P1OUT &= ~0x04;
-      P1OUT &= ~0x08;
-      P1OUT &= ~0x10;
-      step++;
-      counter++;
-    }
-    else if(step == 2){
-      P1OUT &= ~0x02;
-      P1OUT |= 0x04;
-      P1OUT &= ~0x08;
-      P1OUT &= ~0x10;
-      step++;
-      counter++;
-    }
-    else if(step == 3){
-      P1OUT &= ~0x02;
-      P1OUT &= ~0x04;
-      P1OUT |= 0x08;
-      P1OUT &= ~0x10;
-      step++;
-      counter++;
-    }
-    else if(step == 4){
-      P1OUT &= ~0x02;
-      P1OUT &= ~0x04;
-      P1OUT &= ~0x08;
-      P1OUT |= 0x10;
-      step = 1;
-      counter++;
-    }
-  }                                   //end program
-  P1OUT &= ~0x02;
-  P1OUT &= ~0x04;
-  P1OUT &= ~0x08;
-  P1OUT &= ~0x10;
-}*/
+void main(void){
+  WDTCTL = WDTPW | WDTHOLD;                       //Stop Watchdog Timer
+  P1DIR |= 0x02;                                  //configure p1.1 P3 as output step1
+  P1DIR |= 0x04;                                  //configure p1.2 P4 as output step2
+  P1DIR |= 0x08;                                  //configure p1.3 P5 as ouptut step3
+  P1DIR |= 0x10;                                  //configure p1.4 P6 as output step4
+  P1DIR |= 0x20;                                  //configure p1.5 P7 as output enable first pair
+  P1DIR |= 0x40;                                  //configure p1.6 p8 as output select direction of first pair
+  P1DIR |= 0x80;                                  //configure p1.7 p9 as output enable second pair
+  P2SEL = 0;
+  P2DIR |= 0x80;                                  //configure p2.7 p12 as output select direction of second pair
+  P1OUT &= ~0x02;  
+  P1OUT &= ~0x04;                            
+  P1OUT &= ~0x08;                                 
+  P1OUT &= ~0x10;                                  
+  P1OUT &= ~0x20;                                 
+  P1OUT &= ~0x40;                                  
+  P1OUT &= ~0x80;
+  P2OUT &= ~0x80; 
+  move(NorthWest);
+  wait();
+  move(SouthEast);
+  wait();
+  move(NorthEast);
+  wait();
+  move(SouthWest);
+  wait();
+  move(North);
+  wait();
+  move(South);
+  wait();
+  move(West);
+  wait();
+  move(East);
+}
